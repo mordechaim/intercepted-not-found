27cf5b5ddd4fa7f496a14c79f57a38fe6e4f48fe
Mordechai Meisels <mordechai@codaworks.com>
2024-01-21 14:41:03 -0500

    Add dummy action

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,6 +3,9 @@
 import { revalidateTag } from 'next/cache'
 import { cookies } from 'next/headers'
 
+export const dummy = async () => {
+}
+
 export const setCookie = async () => {
     cookies().set('login', Date.now())
 }
--- src/app/login/form.js
+++ src/app/login/form.js
@@ -1,8 +1,9 @@
-import { revalidate, setCookie } from '../actions'
+import { dummy, revalidate, setCookie } from '../actions'
 
 export const LoginForm = () => {
     return <form className='flex flex-col items-start gap-3'>
-        <button className='hover:underline' formAction={setCookie}>Log in (set cookies)</button>
+        <button className='hover:underline' formAction={dummy}>Log in (dummy)</button>
+        <button className='hover:underline' formAction={setCookie}>Log in (set cookie)</button>
         <button className='hover:underline' formAction={revalidate}>Log in (revalidate tag)</button>
     </form>
 }
